7be6aac73a7631f7fe800669f7d170217ec2a242
LocationNotFound <ethanliu186@gmail.com>
2026-02-19 16:55:41 -0500

    Implement key length check and import statements
    Added error handling for key length and imported necessary modules.

--- typemoji.py
+++ typemoji.py
@@ -1,6 +1,11 @@
+from settings import *
+
+from Errors import KeyToLongError
+
+
 def get_emoji(key):
     global emoji_list
-    emoji_list = {"waving_hand":"👋", 
+    emoji_list = {"waving_hand":"👋",
         "clapping_hands":"👏",
         }
     try:
@@ -12,10 +17,18 @@ def get_emoji(key):
 class emoji_printer():
     #for less typing
     def type(filler, key:str):
+        if len(key) > KEY_MAX_LENGTH:
+            raise KeyToLongError("Key is too long")
         print(get_emoji(key))
 
     def return_ascii(filler, key:str):
         return ord(get_emoji(key))
+
+    #currently in progress
+    def change_max_length(filler, new_length:int):
+        debug.change_key_max_length(new_length)
+
+
 
 
 

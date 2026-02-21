0742032cdf62b2b931f80a82a0c5c02d06e1e8a5
LocationNotFound <ethanliu186@gmail.com>
2026-02-20 20:07:58 -0500

    Add emoji list for hand gestures
    Added a dictionary of emoji representations for various hand gestures.

--- Emojilist/lists.py
+++ Emojilist/lists.py
@@ -1,2 +1,16 @@
 #Just a list for all the emojis. Is also implemented in the typemoji.py file until version 0.1.4
 
+emoji_list = {"waving_hand":"👋",
+        "clapping_hands":"👏",
+        "raised_hand":"✋",
+        "victory_hand":"✌️",
+        "pinched_fingers":"🤌",
+        "korean_finger_heart":"🫰",
+        "vulcan_salute":"🖖",
+        "ok_hand":"👌",
+        "heart_hands":"🫶",
+        "thumbs_up":"👍",
+        "thumbs_down":"👎",
+        "crossed_fingers":"🤞"
+        }
+
